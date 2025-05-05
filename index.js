e88e987ffd84d4cda3f160a2aaae04c32524c022
beginningdev <beginningdev01@gmail.com>
2025-05-05 03:35:17 +0000

    Edited

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function greet(name) {
-    console.log("Hello, ".concat(name, "!"));
+//hàm tính tổng các sóng trong mảng
+function sumArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
 }
-greet("Visual Studio Code");
+//mảng số ví dụ
+const numbers = [1, 2, 3, 4, 5];
+//Gọi hàm và in kết quả
+const result = sumArray(numbers);
+console.log("Tổng các số trong mảng là: " + result); // Kết quả: 15
